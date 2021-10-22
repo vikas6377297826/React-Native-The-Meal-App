7d952247b0f2a6df24574e9c7a83bf951fa65263
vikas <vikaspatelkherad@gmail.com>
2021-10-21 22:57:12 -0700

    Configuring the Header with Navigation Options

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,18 +6,28 @@ import {
   Text,
   FlatList,
   ScrollView,
+  TouchableOpacity,
+  Platform,
 } from "react-native";
+import Colors from "../constants/Colors";
 import { CATEGORIES } from "../data/dummy-data";
 
-const renderGridItem = (itemData) => {
-  return (
-    <View style={styles.gridItem}>
-      <Text>{itemData.item.title}</Text>
-    </View>
-  );
-};
-
 const CategoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => {
+          props.navigation.navigate({ routeName: "CategoryMeals" });
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <FlatList
       keyExtractor={(item, index) => item.id}
@@ -26,6 +36,15 @@ const CategoriesScreen = (props) => {
       numColumns={2}
     />
   );
+};
+
+// YE Screen KE Titile ko Change or Style karne ke lie use kia jata hai
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "white",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
